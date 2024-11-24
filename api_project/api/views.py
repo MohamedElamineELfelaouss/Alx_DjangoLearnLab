52a19a7ab523d88ca39d76eb8ce7d6853f6cf279
EL Felaouss mohamed elamine <elfelaoussmohamedelamine@gmail.com>
2024-11-24 22:05:31 +0100

    edited bookview

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,4 @@
-
-#Using a simple view
+# Using a simple view
 
 from django.shortcuts import render
 from .models import Book
@@ -9,27 +8,28 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+
 # Create your views here.
-#Setting up a view that uses the serializer to retrieve and return book data.
-class BookListAPIView(generics.ListAPIView):
-    #implementing authentication
+# Setting up a view that uses the serializer to retrieve and return book data.
+class BookList(generics.ListAPIView):
+    # implementing authentication
     authentication_classes = [TokenAuthentication]
-    #ensuring only authenticated users can access the data from this view
-    permission_classes = [IsAuthenticated]   
+    # ensuring only authenticated users can access the data from this view
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-# Implementing CRUD Operations with ViewSets and Routers 
+
+# Implementing CRUD Operations with ViewSets and Routers
 # in Django REST Framework using ViewSets
 # Create your views here.
 
+
 class BookViewSet(viewsets.ModelViewSet):
-    #authenticating users before they can perform CRUD operations
+    # authenticating users before they can perform CRUD operations
     authentication_classes = [TokenAuthentication]
-   
-   #ensuring only authenticated users can access the data from this view
+
+    # ensuring only authenticated users can access the data from this view
     permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
- 
-
